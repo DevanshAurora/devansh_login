ca0d6d77d86874f106634b5a2f9d0a6b1f48503e
SGt Original <sgtoriginal@gmail.com>
2026-04-29 11:36:25 +0530

    Fixed Initial View

--- my_practice/src/Dashboard.jsx
+++
@@ -1,9 +0,0 @@
-function Dashboard({user,setUser}){
-    return(
-        <div>
-            <h1>Welcome {user}</h1>
-            <button onClick={()=> setUser(null)}>Logout</button>
-        </div>
-    )
-}
-export default Dashboard;
